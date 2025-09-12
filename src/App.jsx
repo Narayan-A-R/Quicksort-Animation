39e89141bdd01d066ddff783786fc0aa954e358e
Narayan-A-R <naratnaparkhe@gmail.com>
2025-09-12 16:07:48 +0530

    website layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,23 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='ViewWindow'>
+        <div className="arrayContainer">
+          <div className="arrayBox">1</div>
+          <div className="arrayBox">2</div>
+          <div className="arrayBox">3</div>
+          <div className="arrayBox">4</div>
+          <div className="arrayBox">5</div>
+          <div className="arrayBox">6</div>
+          <div className="arrayBox">7</div>
+        </div>
+
+        <div className="infoContainer">
+          <div className="helper pseudocode"></div>
+          <div className="helper controls"></div>
+          <div className="helper indices"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
