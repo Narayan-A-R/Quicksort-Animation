83c67a67879ba084665e1a43cf42cb92023ffb63
Narayan-A-R <naratnaparkhe@gmail.com>
2025-10-20 12:50:15 +0530

    added swaping without moving up right then down

--- src/App.jsx
+++ src/App.jsx
@@ -19,36 +19,72 @@ function App() {
 
   const [positions, setPositions] = useState(
     Array.from({ length: n }, (_, i) => ({
+      id: i,
       x: horizRoom + i * arrBoxHeight + i * gapHorz,
       y: vertRoom + arrBoxHeight + gapVert,
     }))
   );
 
   const moveUp = (i) => {
-    setPositions((positions) => positions.map((pos,ind)=>
-      ind===i?{x:pos.x,y:pos.y-(gapVert+arrBoxHeight)}:pos
-    ))
+    setPositions((positions) =>
+      positions.map((pos, ind) =>
+        ind === i ? { x: pos.x, y: pos.y - (gapVert + arrBoxHeight) } : pos
+      )
+    );
   };
 
+  const moveDown = (i) => {
+    setPositions((positions) =>
+      positions.map((pos, ind) =>
+        ind === i ? { x: pos.x, y: pos.y + (gapVert + arrBoxHeight) } : pos
+      )
+    );
+  };
+
+  const moveRight = (i) => {
+    setPositions((positions) =>
+      positions.map((pos, ind) =>
+        ind === i ? { x: pos.x + (gapHorz + arrBoxHeight), y: pos.y } : pos
+      )
+    );
+  };
+
+  const moveLeft = (i) => {
+    setPositions((positions) =>
+      positions.map((pos, ind) =>
+        ind === i ? { x: pos.x - (gapHorz + arrBoxHeight), y: pos.y } : pos
+      )
+    );
+  };
+
+  const swap = (a = 0, b = 1) => {
+    setPositions((prevPositions) => {
+      const copy = [...prevPositions];
+      [copy[a], copy[b]] = [copy[b], copy[a]];
+      return copy;
+    });
+  };
   const boxes = positions.map((pos, i) => (
     <div
-        key={i}
-        className="arrayBox"
-        style={{
-          top: `${pos.y}px`,
-          left: `${pos.x}px`,
-        }}
-        onClick={()=>moveUp(i)}
-      >
-        {i + 1}
-      </div>
+      key={i}
+      className="arrayBox"
+      style={{
+        top: `${pos.y}px`,
+        left: `${pos.x}px`,
+      }}
+      onClick={() => moveUp(i)}
+    >
+      {i + 1}
+    </div>
   ));
   return (
     <>
       <div className="ViewWindow">
         <div className="visuals">{boxes}</div>
         <div className="infoContainer">
-          <div className="informer pseudocode"></div>
+          <div className="informer pseudocode">
+            <button onClick={() => swap(0, 1)} />
+          </div>
           <div className="informer controls"></div>
           <div className="informer indices"></div>
         </div>
