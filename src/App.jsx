ab6a6b46c17e19f89b56bb06cf851993e3181204
Narayan-A-R <naratnaparkhe@gmail.com>
2025-10-21 13:13:04 +0530

    swap with stepped animation

--- src/App.jsx
+++ src/App.jsx
@@ -25,71 +25,91 @@ function App() {
     }))
   );
 
-  const moveUp = (i) => {
-    setPositions((positions) =>
-      positions.map((pos, ind) =>
-        ind === i ? { x: pos.x, y: pos.y - (gapVert + arrBoxHeight) } : pos
-      )
-    );
+  const moveUp = (pos) => ({
+    ...pos,
+    y: pos.y - (gapVert + arrBoxHeight),
+  });
+
+  const moveDown = (pos) => ({
+    ...pos,
+    y: pos.y + (gapVert + arrBoxHeight),
+  });
+
+  const moveRight = (pos) => ({
+    ...pos,
+    x: pos.x + (gapHorz + arrBoxHeight),
+  });
+
+  const moveLeft = (pos) => ({
+    ...pos,
+    x: pos.x - (gapHorz + arrBoxHeight),
+  });
+
+  const swap = (a, b) => {
+    const steps = [
+      (p) =>
+        p.map((pos, i) =>
+          i === a
+            ? { ...pos, y: pos.y - (gapVert + arrBoxHeight) }
+            : i === b
+            ? { ...pos, y: pos.y + (gapVert + arrBoxHeight) }
+            : pos
+        ),
+      (p) =>
+        p.map((pos, i) =>
+          i === a
+            ? { ...pos, x: pos.x + (gapHorz + arrBoxHeight) }
+            : i === b
+            ? { ...pos, x: pos.x - (gapHorz + arrBoxHeight) }
+            : pos
+        ),
+      (p) =>
+        p.map((pos, i) =>
+          i === a
+            ? { ...pos, y: pos.y + (gapVert + arrBoxHeight) }
+            : i === b
+            ? { ...pos, y: pos.y - (gapVert + arrBoxHeight) }
+            : pos
+        ),
+    ];
+
+    const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    (async () => {
+      for (const step of steps) {
+        setPositions((prev) => step(prev));
+        await sleep(250);
+      }
+    })();
   };
 
-  const moveDown = (i) => {
-    setPositions((positions) =>
-      positions.map((pos, ind) =>
-        ind === i ? { x: pos.x, y: pos.y + (gapVert + arrBoxHeight) } : pos
-      )
-    );
-  };
-
-  const moveRight = (i) => {
-    setPositions((positions) =>
-      positions.map((pos, ind) =>
-        ind === i ? { x: pos.x + (gapHorz + arrBoxHeight), y: pos.y } : pos
-      )
-    );
-  };
-
-  const moveLeft = (i) => {
-    setPositions((positions) =>
-      positions.map((pos, ind) =>
-        ind === i ? { x: pos.x - (gapHorz + arrBoxHeight), y: pos.y } : pos
-      )
-    );
-  };
-
-  const swap = (a = 0, b = 1) => {
-    setPositions((prevPositions) => {
-      const copy = [...prevPositions];
-      [copy[a], copy[b]] = [copy[b], copy[a]];
-      return copy;
-    });
-  };
-  const boxes = positions.map((pos, i) => (
+  const boxes = positions.map((pos) => (
     <div
-      key={i}
+      key={pos.id}
       className="arrayBox"
       style={{
         top: `${pos.y}px`,
         left: `${pos.x}px`,
       }}
-      onClick={() => moveUp(i)}
+      onClick={() =>
+        setPositions((ps) => ps.map((p) => (p.id === pos.id ? moveUp(p) : p)))
+      }
     >
-      {i + 1}
+      {pos.id + 1}
     </div>
   ));
+
   return (
-    <>
-      <div className="ViewWindow">
-        <div className="visuals">{boxes}</div>
-        <div className="infoContainer">
-          <div className="informer pseudocode">
-            <button onClick={() => swap(0, 1)} />
-          </div>
-          <div className="informer controls"></div>
-          <div className="informer indices"></div>
+    <div className="ViewWindow">
+      <div className="visuals">{boxes}</div>
+      <div className="infoContainer">
+        <div className="informer pseudocode">
+          <button onClick={() => swap(0, 1)}>Swap</button>
         </div>
+        <div className="informer controls"></div>
+        <div className="informer indices"></div>
       </div>
-    </>
+    </div>
   );
 }
 
